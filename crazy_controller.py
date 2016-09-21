3807a029831cd2d23e58d7e333014851f719420b
Adam Spontarelli <adam@vector-space.org>
2016-09-21 00:56:02 +0000

    cleanup

--- crazy_controller.py
+++ crazy_controller.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python
- 
-# Example for RC timing reading for Raspberry Pi
-# Must be used with GPIO 0.3.1a or later - earlier verions
-# are not fast enough!
+# RVA Makerfest RetroPi controller
+# Adam, Justin
  
 import RPi.GPIO as GPIO, time, os
 import random
 
-DEBUG = 1
+light_sensor_pin = 18
+servo_pin = 12
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.OUT)
-pwm = GPIO.PWM(12, 50)
-pwm.start(5)
+GPIO.setup(servo_pin, GPIO.OUT)
+pwm = GPIO.PWM(servo_pin, 50)
  
 def RCtime (RCpin):
-        reading = 0
-        GPIO.setup(RCpin, GPIO.OUT)
-        GPIO.output(RCpin, GPIO.LOW)
-        time.sleep(0.1)
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.1)
  
-        GPIO.setup(RCpin, GPIO.IN)
-        # This takes about 1 millisecond per loop cycle
+    GPIO.setup(RCpin, GPIO.IN)
+    # This takes about 1 millisecond per loop cycle
         
-        while (GPIO.input(RCpin) == GPIO.LOW):
-                reading += 1
+    while (GPIO.input(RCpin) == GPIO.LOW):
+        reading += 1
 
-        if reading < 300:
-                print('hit')
-        return reading
+    if reading < 300:
+        print('laser hit')
+    return reading
 
- 
-while True:                                     
-        RCtime(18)     # Read RC timing using pin #18
+
+def main():
+    while True:                                     
+        RCtime(light_sensor_pin)     # Read RC timing using pin #18
         x = random.randrange(100)
-	pwm.start(x)
-	time.sleep(0.1)
-
+    	pwm.start(x)
+    	time.sleep(0.1)
+    
+if __name__ == '__main__':
+    main()
