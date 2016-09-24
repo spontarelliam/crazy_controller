b69102046a990a4afbb885380928f13e4499260e
Adam Spontarelli <spontarelliam@gmail.com>
2016-09-23 21:07:44 -0400

    minor cleanup

--- load_cell.py
+++ load_cell.py
@@ -17,19 +17,16 @@ while True:
 	sleep(0.1)
 	try:
 		val = (0.5 * val) + (0.25 * bus.read_byte_data(0x28, 0x80))
-                # nominal = 40
-                print(val)
 		if val > 60:
-			print "Right"
                         GPIO.output(D_PIN, 0)
                         time.sleep(0.1)
                         GPIO.output(D_PIN, 1)
 		elif val < 20:
-			print "Left"
                         GPIO.output(S_PIN, 0)
                         time.sleep(0.1)
                         GPIO.output(S_PIN, 1)
 		else:
-			print "Center"
+                        # center
+                        pass
 	except:
 		pass
